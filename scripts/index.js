991d9ee529f3bbaa0edf1d1705d9d0577713f3e2
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-12-21 15:15:24 -0600

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentloaded", e =>{
-    
-})
+document.addEventListener("DOMContentLoaded", e => {
+
+    loginButton.addEventListener("click", async e => {
+        localStorage.token = await api.login({ 
+            username: username.value, 
+            password: password.value 
+        });
+
+        window.location = "posts.html"
+    });
+
+});
