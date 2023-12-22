02685ca7d98a9e5e523f88a3d1442d10256098e3
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-12-22 10:45:05 -0600

    trying to fix pull  error

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,51 +1,81 @@
-function getMessageHTML(m) {
-    return `
-        <fieldset id="messageTemplate">
-        <legend>Message Template</legend>
-        <label>
-            <div class="name">Username:</div><input readonly="readonly" value="${m.username}">
-        </label>
-        <label>
-            <div class="name">Text:</div><input readonly="readonly" value="${m.text}">
-        </label>
-        <label>
-            <div class="name"></div><input type="button" value="LIKE" onclick="like('${m._id}')">
-            ${m.likes.length} likes.
-        </label>
-        </fieldset>
-    `;
-}
-async function post(messageText){
-    await api.post("/api/posts", {text: messageText}, localStorage.token);
-    showMessages(localStorage.username)
-    userList.value = localStorage.username;
-}
+data = [{
+    "_id": "65837c845f43a2d27769d078",
+    "text": "Another post",
+    "username": "test123",
+    "createdAt": "2023-12-20T23:45:08.054Z",
+    "likes": []
+}]
+data.forEach(item => console.log(item.username));
 
-function like(id){
-    api.post("/api/likes", {postId: id}, localStorage.token);
-}
-
-async function showMessages(username = "") {
-    let url = "/api/posts"
-    messages = await api.get(url, localStorage.token);
-    if (username != "") {
-        messages = messages.filter(m=>m.username = username)
+//USER LIST
+users = [
+    {
+        "fullName": "Patrick Starfish",
+        "username": "patpat",
+        "bio": "",
+        "createdAt": "2023-12-20T17:40:28.808Z",
+        "updatedAt": "2023-12-20T17:40:28.808Z"
+    },
+    {
+        "fullName": "Fred",
+        "username": "fflintstone",
+        "bio": "my ip is 221.180.76.56 ",
+        "createdAt": "2023-12-20T17:44:54.755Z",
+        "updatedAt": "2023-12-20T19:30:53.412Z"
     }
-    results.innerHTML = messages.map(getMessageHTML).join("")
-}
+]
+users.forEach(item => console.log(item.fullName))
+//REFRESH TOKEN
+input = {
+    "username": "string",
+    "password": "string"
+};
+host = "http://microbloglite.us-east-2.elasticbeanstalk.com"
+fetch(host + "/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(input)
+})
+    .then(response => response.json())
+    .then(output => {
+        //TODO set session rembmer that we are logged in
+        // localStorage.token = output.token;
+        // localStorage.username = output.username;
+        // document.body.innerHTML += `<pre>` + JSON.stringify(output,0,4) + `</pre>`;
+        token = output.token;
+        // posts();//ask only after login
 
-function getUserOption(u){
-    return `<option>${u.username}</option>`
-}
+        //token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTcwMzE3MTQ4NCwiZXhwIjoxNzAzMjU3ODg0fQ.WlBmOJK7bk4L5MknRCH8iDTTju9RgCv88sovLybYkNY"
 
-document.addEventListener("DOMContentLoaded", async e => {
-    const users = await api.get("/api/users", localStorage.token)
-    userList.innerHTML += users.map(getUserOption).join("")
+        fetch(host + "/api/users", {
+            method: "GET",
+            headers: { 'Authorization': `Bearer ${token}` }
+        }).then(response => response.json())
+            .then(data => { //console.log(data);TEST
+                data.forEach(item => {
+                    userList.innerHTML +=`<option>${item.fullName}</option>`
+                })
+            })
 
-    showMessages(); //ALL
+            //
 
-    userList.addEventListener("change", async e => {
-        showMessages(userList.selectedOptions[0].value)
+        fetch(host + "/api/posts", {
+            method: "GET",
+            headers: { 'Authorization': `Bearer ${token}` }
+        }).then(response => response.json())
+            .then(data => { //console.log(data);TEST
+                data.forEach(showPost)
+            })
     });
+    function showPost(post){
+        Result.innerHTML += `
+        <div class ="post">
+        ${post.username}
+        <button onclick="likePost('${post._id}')">like</button>
+        </div>
+        `
 
-});
+    }
+    function likePost(id){
+        alert(id)
+    }
