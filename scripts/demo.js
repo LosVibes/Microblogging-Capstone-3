d9348ffdf818fd6cab5978705a84ed8c7546429b
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2024-01-05 11:44:47 -0600

    fix

--- scripts/demo.js
+++ scripts/demo.js
@@ -3,11 +3,12 @@
 document.addEventListener("DOMContentLoaded", async () => {
 
   const input = {
-    "username": "username",
-    "password": "password"
+    "username": "spiderman2",
+    "password": "12345678"
   };
 
   const host = "http://microbloglite.us-east-2.elasticbeanstalk.com";
+  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ikxpb24iLCJpYXQiOjE3MDQzOTA2ODcsImV4cCI6MTcwNDQ3NzA4N30.YNjDuVCp_eVwAwITKvretyRKNxRObO8w4xEG_rJVa04";
   try {
     const response = await fetch(`${host}/auth/login`, {
       method: "POST",
